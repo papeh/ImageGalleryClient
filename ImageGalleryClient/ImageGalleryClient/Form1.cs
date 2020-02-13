b127d5f62f34ff74bee15f677ef6e77dd03276a0
John Hatton <hattonjohn@gmail.com>
2020-02-13 15:20:14 -0700

    Move Image control to main screen, add save and clipboard buttons

--- ImageGalleryClient/ImageGalleryClient/Form1.cs
+++ ImageGalleryClient/ImageGalleryClient/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
-using SIL.Windows.Forms.ImageToolbox;
-using SIL.Windows.Forms.ImageToolbox.ImageGallery;
 
 namespace ImageGalleryClient
 {
@@ -10,20 +9,46 @@ namespace ImageGalleryClient
 		public Form1()
 		{
 			InitializeComponent();
-		}
 
-		private void Button1_Click(object sender, EventArgs e)
+			this.clipboardButton.Enabled = this.saveButton.Enabled = false;
+		}
+		void onImageChanged(object sender, EventArgs e)
 		{
-			using (var imageInfo = new PalasoImage())  
-			using (var dlg = new ImageToolboxDialog(imageInfo, null))
+			this.clipboardButton.Enabled = this.saveButton.Enabled = this._imageGalleryControl.GetImage() !=null;
+		}
+		private void OnSaveClick(object sender, EventArgs e)
+		{
+			var filename = this._imageGalleryControl.GetImage().FileName;
+			using (var dialog = new SaveFileDialog
 			{
-				dlg.SearchLanguage = "en";
-				var result = dlg.ShowDialog();
-				if (DialogResult.OK == result && dlg.ImageInfo != null)
+				Filter = "All files (*.*)|*.*",
+				InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+				RestoreDirectory = true,
+				DefaultExt = Path.GetExtension(filename),
+				FileName = filename
+			})
+			{
+				if (dialog.ShowDialog(this) == DialogResult.OK)
 				{
-					MessageBox.Show(dlg.ImageInfo.OriginalFilePath);
+					this._imageGalleryControl.GetImage().Save(dialog.FileName);
 				}
 			}
+		}
+
+		private void OnClipboardClick(object sender, EventArgs e)
+		{
+			Clipboard.SetImage(this._imageGalleryControl.GetImage().Image);
+		}
+
+		private void _checkForCollectionsTimer_Tick(object sender, EventArgs e)
+		{
+			this._checkForCollectionsTimer.Enabled = false;
+			if (!this._imageGalleryControl.HaveImageCollectionOnThisComputer)
+			{
+				this._imageGalleryControl.Visible = false;
+				this._noCollectionsMessage.Visible = true;
+			}
+
 		}
 	}
 }
