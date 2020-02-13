2c8db723859040a4eaf9054e805a5038acf8c7cb
John Hatton <hattonjohn@gmail.com>
2020-02-12 20:21:37 -0700

    Working sample

---
+++ ImageGalleryClient/ImageGalleryClient/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using SIL.Windows.Forms.ImageToolbox;
+using SIL.Windows.Forms.ImageToolbox.ImageGallery;
+
+namespace ImageGalleryClient
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			using (var imageInfo = new PalasoImage())  
+			using (var dlg = new ImageToolboxDialog(imageInfo, null))
+			{
+				dlg.SearchLanguage = "en";
+				var result = dlg.ShowDialog();
+				if (DialogResult.OK == result && dlg.ImageInfo != null)
+				{
+					MessageBox.Show(dlg.ImageInfo.OriginalFilePath);
+				}
+			}
+		}
+	}
+}
