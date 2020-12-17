a22f1cb1fe2fdef23d0a2d809256327f7fe248db
Venu Kommu <itsvenumail@gmail.com>
2020-12-17 14:28:43 +0530

    Merge branch into master

--- ImageGalleryClient/Form1.Designer.cs
+++ ImageGalleryClient/Form1.Designer.cs
@@ -40,23 +40,36 @@
             // 
             // saveButton
             // 
-            resources.ApplyResources(this.saveButton, "saveButton");
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(557, 408);
             this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save...";
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.OnSaveClick);
             // 
             // clipboardButton
             // 
-            resources.ApplyResources(this.clipboardButton, "clipboardButton");
+            this.clipboardButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clipboardButton.Location = new System.Drawing.Point(652, 408);
             this.clipboardButton.Name = "clipboardButton";
+            this.clipboardButton.Size = new System.Drawing.Size(110, 23);
+            this.clipboardButton.TabIndex = 1;
+            this.clipboardButton.Text = "Copy to Clipboard";
             this.clipboardButton.UseVisualStyleBackColor = true;
             this.clipboardButton.Click += new System.EventHandler(this.OnClipboardClick);
             // 
             // _imageGalleryControl
             // 
-            resources.ApplyResources(this._imageGalleryControl, "_imageGalleryControl");
+            this._imageGalleryControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._imageGalleryControl.Location = new System.Drawing.Point(5, 0);
             this._imageGalleryControl.Name = "_imageGalleryControl";
             this._imageGalleryControl.SearchLanguage = "en";
+            this._imageGalleryControl.Size = new System.Drawing.Size(794, 389);
+            this._imageGalleryControl.TabIndex = 2;
             this._imageGalleryControl.ImageChanged += new System.EventHandler(this.onImageChanged);
             // 
             // _checkForCollectionsTimer
@@ -68,12 +81,20 @@
             // _noCollectionsMessage
             // 
             this._noCollectionsMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            resources.ApplyResources(this._noCollectionsMessage, "_noCollectionsMessage");
+            this._noCollectionsMessage.Enabled = false;
+            this._noCollectionsMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._noCollectionsMessage.ForeColor = System.Drawing.SystemColors.ControlText;
             this._noCollectionsMessage.IsTextSelectable = false;
+            this._noCollectionsMessage.Location = new System.Drawing.Point(198, 124);
+            this._noCollectionsMessage.Multiline = true;
             this._noCollectionsMessage.Name = "_noCollectionsMessage";
             this._noCollectionsMessage.ReadOnly = true;
+            this._noCollectionsMessage.Size = new System.Drawing.Size(337, 60);
+            this._noCollectionsMessage.TabIndex = 3;
             this._noCollectionsMessage.TabStop = false;
+            this._noCollectionsMessage.Text = "Please install one or more Bloom-compatible image collections on this computer fi" +
+    "rst, for example https://bloomlibrary.org/artofreading.";
+            this._noCollectionsMessage.Visible = false;
             // 
             // _fixSizingTimer
             // 
@@ -82,13 +103,16 @@
             // 
             // Form1
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this._noCollectionsMessage);
             this.Controls.Add(this._imageGalleryControl);
             this.Controls.Add(this.clipboardButton);
             this.Controls.Add(this.saveButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.Text = "Image Galleries";
             this.ResumeLayout(false);
             this.PerformLayout();
 
